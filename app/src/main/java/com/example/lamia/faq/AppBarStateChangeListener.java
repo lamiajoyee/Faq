b0218c4538a5e9c999b9f3c1b1cd24adcd0d8ed9
Lamia <lamiajoyee@yahoo.com>
2017-05-16 18:33:27 +0600

    extensive advancements

---
+++ app/src/main/java/com/example/lamia/faq/AppBarStateChangeListener.java
@@ -0,0 +1,10 @@
+package com.example.lamia.faq;
+
+import android.support.design.widget.AppBarLayout;
+
+/**
+ * Created by Lamia on 5/16/2017.
+ */
+
+public class AppBarStateChangeListener implements AppBarLayout.OnOffsetChangedListener {
+}
